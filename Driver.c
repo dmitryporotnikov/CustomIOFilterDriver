7d054cf60aa4171ef79d67b274e81c34bad29e5f
dmitryporotnikov <dmitry.porotnikov@gmail.com>
2024-02-01 17:19:39 +0100

    Update Driver.c
    We now distinguish between user mode and kernel mode ;)

--- Driver.c
+++ Driver.c
@@ -1,7 +1,8 @@
 #include <fltKernel.h>
+#include <ntddk.h>
+#include <ntifs.h>
 #include <dontuse.h>
 #include <suppress.h>
-
 #pragma prefast(disable:__WARNING_ENCODE_MEMBER_FUNCTION_POINTER, "Not valid for kernel mode drivers")
 
 PFLT_FILTER gFilterHandle;
@@ -29,28 +30,24 @@ const FLT_REGISTRATION FilterRegistration = {
 
 FLT_PREOP_CALLBACK_STATUS PreOperationCallback(PFLT_CALLBACK_DATA Data, PCFLT_RELATED_OBJECTS FltObjects, PVOID* CompletionContext)
 {
-    UNREFERENCED_PARAMETER(Data);
     UNREFERENCED_PARAMETER(FltObjects);
     UNREFERENCED_PARAMETER(CompletionContext);
 
-    // Get a somewhat random seed based on the current system time
-    LARGE_INTEGER currentTime;
-    KeQuerySystemTime(&currentTime);
-
-    // Use the lower bits of the current time as a simple "random" seed
-    ULONG seed = currentTime.LowPart;
-
-    // Generate a random delay between 0 and 5 seconds
-    // The modulus operator helps ensure the delay is within the 0-5 range
-    int delayInSeconds = seed % 6; // 0 to 5 inclusive
-
-    LARGE_INTEGER delay;
-    delay.QuadPart = -(10 * 1000 * 1000 * (LONGLONG)delayInSeconds); // Convert to 100-nanosecond intervals
-
-    KeDelayExecutionThread(KernelMode, FALSE, &delay);
+    // Check if the operation is initiated by user-mode
+    if (Data->RequestorMode == UserMode) {
+        // This is a user-mode request; introduce delay
+        LARGE_INTEGER currentTime;
+        KeQuerySystemTime(&currentTime);
+        ULONG seed = currentTime.LowPart;
+        int delayInSeconds = seed % 6; // 0 to 5 inclusive
+        LARGE_INTEGER delay;
+        delay.QuadPart = -(10 * 1000 * 1000 * (LONGLONG)delayInSeconds); // Convert to 100-nanosecond intervals
+        KeDelayExecutionThread(KernelMode, FALSE, &delay);
+    }
 
     return FLT_PREOP_SUCCESS_NO_CALLBACK;
 }
+
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
 {
